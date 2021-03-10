f8074b2ef5607b0a35919d4834868ede8000e73b
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-03-10 18:49:30 +0100

    Added label to birth_date

--- pet_food_diary/forms.py
+++ pet_food_diary/forms.py
@@ -6,5 +6,5 @@ from pet_food_diary.models import Veterinarian
 class PetForm(forms.Form):
     name = forms.CharField(max_length=64, label="Pet's name", required=True)
     vet = forms.ModelChoiceField(queryset=Veterinarian.objects.filter(is_vet=True), label="Vet's name")
-    date_of_birth = forms.DateField()
+    date_of_birth = forms.DateField(label="Pet's birth date")
     comments = forms.Textarea()
