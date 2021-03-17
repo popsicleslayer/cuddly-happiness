b90df780b41ee1af3f623266dcfaafb29a391786
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-03-17 14:25:35 +0100

    Updated base.html with content title. Added models to admin.py to make them visible on admin page

--- pet_food_diary/admin.py
+++ pet_food_diary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PetModel, PetFoodModel, PetFoodAmountModel
+admin.site.register(PetModel)
+admin.site.register(PetFoodModel)
+admin.site.register(PetFoodAmountModel)
 
 # Register your models here.
