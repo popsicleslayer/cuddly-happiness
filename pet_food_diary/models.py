6bac10ae8915ae32d0e9a513d0ae2947ffc4cc0c
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-03-17 14:24:09 +0100

    Updated models with get_absolute_url function

--- pet_food_diary/models.py
+++ pet_food_diary/models.py
@@ -9,46 +9,31 @@ class Veterinarian(models.Model):
 class PetModel(models.Model):
     name = models.CharField(max_length=64)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_name')
-    vet = models.ForeignKey(User, default='Null', on_delete=models.SET_DEFAULT, related_name='vet_name')
-    date_of_birth = models.DateField
-    comments = models.TextField
+    # vet = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='vet_name')
+    date_of_birth = models.DateField()
+    comments = models.TextField(null=True)
 
 
 class PetFoodModel(models.Model):
     brand = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
-    proteins = models.FloatField
-    fats = models.FloatField
-    fiber = models.FloatField
-    ash = models.FloatField
-    other = models.TextField
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    fiber = models.FloatField()
+    ash = models.FloatField()
+    other = models.TextField()
+
+    def get_absolute_url(self):
+        return f'/pet_food/{self.id}/'
 
 
 class MealModel(models.Model):
     pet_food_used = models.ManyToManyField(PetFoodModel, through='PetFoodAmountModel')
     pet = models.ForeignKey(PetModel, on_delete=models.CASCADE)
-    date = models.DateTimeField
+    date = models.DateTimeField()
 
 
 class PetFoodAmountModel(models.Model):
     meal = models.ForeignKey(MealModel, on_delete=models.CASCADE)
     pet_food = models.ForeignKey(PetFoodModel, on_delete=models.CASCADE)
-    grams = models.IntegerField
-
-
-class MealPlanModel(models.Model):
-    # TYPE_OF_MEAL_CHOICES=(
-    #     (0, 'breakfast'),
-    #     (1, 'lunch'),
-    #     (2, 'dinner')
-    # )
-    pet_food = models.ForeignKey(PetFoodModel, on_delete=models.CASCADE)
-    pet = models.OneToOneField(PetModel, on_delete=models.CASCADE)
-    grams = models.IntegerField
-    # type_of_meal = models.IntegerField(choices=TYPE_OF_MEAL_CHOICES)
-
-
-# class MessagesModel(models.Model):
-#     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipient')
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-#     message = models.TextField
+    grams = models.IntegerField()
